16c2aaf0e13586ca4467295ad4b664f2ed9e3540
jarekkol <jarekkol@o2.pl>
2020-09-01 19:46:05 +0200

    Update main.tf

--- ec2_machine/main.tf
+++ ec2_machine/main.tf
@@ -32,7 +32,7 @@ resource "aws_instance" "example" {
 
   user_data = <<-EOF
               #!/bin/bash
-              echo "Hello, World" > index.html
+              echo "Hello, World!" > index.html
               nohup busybox httpd -f -p "${var.server_port}" &
               EOF
 
